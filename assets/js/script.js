39503c432f892346dbb01fd05af85550a905a306
danielj314 <danieljones314@gmail.com>
2022-04-19 16:07:53 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// Wait for DOM to finnish loading before running the gale
+// Get the button elements and add event listeners to the game
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
@@ -16,20 +19,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 })
 
-
-
-
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
-    // Creates two random numbers between 1 and 25
+
+// Creates two random numbers between 1 and 25
 let num1 = Math.floor(Math.random() * 25) + 1;
 let num2 = Math.floor(Math.random() * 25) + 1;
 
 if(gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+} else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
 } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -70,7 +73,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    }else {
         alert("Unimplemented operator ${operator}");
         throw "Unimplemented operator ${operator}, Aborting!";
     }
@@ -106,8 +111,10 @@ function displaySubtractQuestion() {
     
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
 function displayDivisionQuestion() {
