cd36188911b43c771fb000e7e32e65375ec7eaf3
danielj314 <danieljones314@gmail.com>
2022-04-19 17:05:13 +0000

    Division question code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,7 +139,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionQuestion() {
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
     
 
 
